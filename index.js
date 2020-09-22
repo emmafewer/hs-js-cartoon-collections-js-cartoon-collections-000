344e9b2914acb20745c4b2ff2d446f94998a4909
Emma Fewer <emmafewer20@gmail.com>
2020-09-22 18:12:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function dwarfRollCall(dwarves) {
+  var dwarfString = [];
+  for (let i=0; i<dwarves.length; i++) {
+    dwarfString.push(`${i+1}. ${dwarves[i]} `)
+  }
+  return `${dwarfString.join("")}`
+}
+dwarfRollCall()
+
+
+function summonCaptainPlanet(planeteerCalls) {
+  var planeteerString = [];
+  for (let i=0; i<planeteerCalls.length; i++) {
+  planeteerString.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+  return planeteerString
+}
+summonCaptainPlanet()
+
+
+function longPlaneteerCalls(words){
+  for(let i=0; i<words.length; i++) {
+    if (words[i].length>4){
+      return true
+    } else {
+    return false
+    }
+  }
+}
+longPlaneteerCalls()
+
+
+function findTheCheese(foods) {
+  for(let i=0; i<foods.length; i++){
+    if (foods[i]==="cheddar"||foods[i]==="gouda"||foods[i]==="camembert"){
+    return foods[i]
+    }
+  }
+  return "no cheese!"
 }
 
-function summonCaptainPlanet(planeteerCalls){
-}
-
-function longPlaneteerCalls(words) {
-}
-
-function findTheCheese (foods) {
-}
